0e79cbf43c681d3558d5277e98241bd6c0ec8737
«Julia» <jul_an@mail.ru>
2022-12-22 21:17:30 +0300

    Homework 6

--- Program.cs
+++ Program.cs
@@ -441,3 +441,90 @@ ShowArray(myArray);
 DiffArray(myArray);
 
 */
+
+//ДЗ 6.
+
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+/*
+int[] CreateArray(int size)
+{
+    int[] array = new int[size]; 
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input numbers of array for index {i + 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }    
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " "); 
+    Console.WriteLine();
+}
+
+void PositiveNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+
+            count++;
+        
+    }
+    Console.WriteLine($"Number of positive is {count} elements");
+}
+
+Console.Write("Input a number of elements: ");
+int array = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateArray(array);
+ShowArray(myArray);
+PositiveNum(myArray);
+
+*/
+
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine()); 
+
+double x = 0;
+double y = 0;
+     
+if (k1 != k2) 
+{  
+    x = -(b1 - b2) / (k1 - k2);
+    y = k1 * x + b1;
+ 
+    x = Math.Round(x, 1);
+    y = Math.Round(y, 1);
+
+    Console.WriteLine($"Точка пересечения двух прямых ({x}; {y})");
+}
+
+else
+
+if (b1 == b2 && k1 == k2)
+    Console.WriteLine("Прямые совпадают.");
+
+ 
+
+
+
+
+
