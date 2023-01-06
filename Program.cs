d73a549d5848a898f99dd9d7972b9f67daea8e10
«Julia» <jul_an@mail.ru>
2023-01-06 16:58:03 +0300

    Homework 7

--- Program.cs
+++ Program.cs
@@ -579,7 +579,7 @@ Console.WriteLine(sum);
 
 
 //Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-
+/*
 int AkkermanFunction(int m, int n)
 {
     if (m == 0)
@@ -602,13 +602,168 @@ Console.Write("Input a number N: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 int function = AkkermanFunction (num1, num2);
 Console.WriteLine(function);
+*/
+
+// ДЗ 7
+
+//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. 
+/*
+double[,] CreateRandom2dArray()
+{
+    Console.Write("Input a number of rows:");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns:");
+    int columns = Convert.ToInt32(Console.ReadLine()); 
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    double[,] array = new double[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    
+        for(int j = 0; j < columns; j++)
+        
+            array[i,j] = Math.Round(new Random().Next(minValue, maxValue + 1)+ new Random().NextDouble(), 2);
 
 
+        return array;
+    
+
+}
+void Show2dArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + "\t");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+
+*/
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+/*
+
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input a number of rows:");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns:");
+    int columns = Convert.ToInt32(Console.ReadLine()); 
+    
+
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    
+        for(int j = 0; j < columns; j++)
+        
+            array[i,j] = new Random().Next(1, 10);
+
+        return array;
+    
+
+}
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + "\t");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ShowElementArray (int[,] array)
+{
+    Console.Write("Input a rows number: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a columns number: ");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    if (x <= array.GetLength(0) && y <= array.GetLength(1))
+    {
+        Console.Write(array[x,y] + "\t");
+        
+    }
+    else
+       Console.WriteLine("No such element.");
+
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+ShowElementArray(myArray);
+
+*/
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
 
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input a number of rows:");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns:");
+    int columns = Convert.ToInt32(Console.ReadLine()); 
+    
+
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    
+        for(int j = 0; j < columns; j++)
+        
+            array[i,j] = new Random().Next(1, 10);
+
+        return array;
+    
+
+}
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + "\t");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
 
 
+double[] ArithmeticMean(int [,] array) 
+{
+    double[] sumColumns = new double[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(1); i++)
+        {
+            sumColumns[i] = 0;
+            for (int j = 0; j < array.GetLength(0); j++)
+            {
+                sumColumns[i] += array[j,i];
+            }  
+            sumColumns[i] = Math.Round(sumColumns[i] / array.GetLength(0), 2);
+            Console.Write($"{sumColumns[i]}\t");
+        }
+        return sumColumns;
+}
 
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+double[] array = ArithmeticMean(myArray); 
 
 
 
